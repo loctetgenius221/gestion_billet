4bff1167cad7ba0e33f437ae48ed184ad35fe23b
loctetgenius <sagnamoussa546@gmail.com>
2024-03-05 13:58:04 +0000

    Vérification que la connexion marche

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $bdd = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 if(!$bdd) {
     die("La connexion à échouée! : " . mysqli_connect_error());
 } else {
-    echo "Connexion réussie avec succès";
+    // echo "Connexion réussie avec succès";
 }
 
 
