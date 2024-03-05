d4dd40655d0972345ab62fc5ae60eec14c7c5dc0
loctetgenius <sagnamoussa546@gmail.com>
2024-03-05 12:49:57 +0000

    Établissement de la connexion avec la base de données

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+define("SERVERNAME", "localhost");
+define("USERNAME", "root");
+define("PASSWORD", "");
+define("DBNAME", "gestion_billet");
+
+$bdd = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+
+if(!$bdd) {
+    die("La connexion à échouée! : " . mysqli_connect_error());
+} else {
+    echo "Connexion réussie avec succès";
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
