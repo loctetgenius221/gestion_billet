d640c12e629e3513d63333a9654fcbe6197032c5
loctetgenius <sagnamoussa546@gmail.com>
2024-03-06 12:41:35 +0000

    Ajout du header et main.css pour la mise en place et le style

---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+    <!-- Voici mon header entete qui sera sur toute les pages -->
+    <header class="header">
+        <h1 class="header-logo">
+            <span>S</span>agna <span>V</span>oyage<span>.</span>
+        </h1>
+        <nav class="navbar">
+            <ul>
+                <li><a href="#" class="active"><i class="fa-solid fa-house"></i>Accueil</a></li>
+                <li><a href="#"><i class="fa-solid fa-plane-departure"></i>Réservation</a></li>
+                <li><a href="#"><i class="fa-solid fa-user"></i>Client</a></li>
+            </ul>
+        </nav>
+        <!--  -->
+    </header>
+</body>
+</html>
